2c2ffd93c7993c8f8eff38031f0748d1be0135b2
Shubham kumar Gupta <shubhamgupta309@gmail.com>
2024-03-05 22:11:01 +0530

    add composer

--- config.php
+++ config.php
@@ -4,12 +4,34 @@ if(isset($_GET['debug'])){
     ini_set('display_startup_errors', 1);
     error_reporting(E_ALL);
 }
+
+// require_once __DIR__ . '/resources/vendor/autoload.php';
+
+// use Dotenv\Dotenv;
+
+// $dotenv = Dotenv::createImmutable(__DIR__);
+// $dotenv->load();
+
+// print_r($_SERVER);  //worked
+// print_r(getenv());  //worked
+
+$envFile = __DIR__ . '/.env';
+if (file_exists($envFile)) {
+    $env = parse_ini_file($envFile);//use file()
+    foreach ($env as $key => $value) {
+        putenv("$key=$value");
+    }
+}
+else{
+  die("sorry .env file not found");  
+}
+
 session_start();
 
-define("DB_HOSTNAME", "localhost" );
-define("DB_USERNAME", "root" );
-define("DB_PASSWORD", "" );
-define("DB_", "client_bollyhood" );
+define("DB_HOSTNAME", getenv('DB_HOST'));
+define("DB_USERNAME", getenv('DB_USERNAME'));
+define("DB_PASSWORD", getenv('DB_PASSWORD'));
+define("DB_",getenv('DB_DATABASE'));
 
 $mysqli = new mysqli(DB_HOSTNAME,DB_USERNAME,DB_PASSWORD,DB_);
 // Check connection
